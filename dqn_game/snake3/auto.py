ddef42f78071cd6fb55c146405ecb64fee1fdfd4
sunshaolei <softking.top@gmail.com>
2018-09-06 18:47:52 +0800

    update

---
+++ dqn_game/snake3/auto.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import numpy as np
+import random
+from collections import deque
+import gym
+import time
+import math
+
+GAMMA = 0.9  # discount factor for target Q
+INITIAL_EPSILON = 0.02  # starting value of epsilon
+FINAL_EPSILON = 0.001  # final value of epsilon
+REPLAY_SIZE = 10000  # 经验回放缓存大小
+BATCH_SIZE = 200  # 小批量尺寸
+TARGET_Q_STEP = 100  # 目标网络同步的训练次数
+
+
+class DQN():
+    # DQN Agent
+    def __init__(self, env):
+        # init experience replay
+        self.replay_buffer = deque()
+        # init some parameters
+        self.time_step = 0
+        self.epsilon = INITIAL_EPSILON
+        self.state_dim = 200
+        self.action_dim = 4
+        self.hide_layer_inputs = 1000 
+        # 创建Q网络
+        self.create_Q_network()
+        # 创建训练方法
+        self.create_training_method()
+
+        self.target_q_step = TARGET_Q_STEP
+        self.create_TargetQ_network()
+
+        # 初始会话
+        self.session = tf.InteractiveSession()
+        self.session.run(tf.initialize_all_variables())
+
+        # saving and loading networks
+        self.saver = tf.train.Saver()
+        self.session.run(tf.initialize_all_variables())
+        checkpoint = tf.train.get_checkpoint_state("models")
+        if checkpoint and checkpoint.model_checkpoint_path:
+            self.saver.restore(self.session, checkpoint.model_checkpoint_path)
+            print("Successfully loaded:", checkpoint.model_checkpoint_path)
+        else:
+            print("Could not find old network weights")
+
+    def save_models(self, times):
+        self.saver.save(self.session, 'models/snake', global_step=times)
+
+    def create_Q_network(self):
+        # network weights
+        W1 = self.weight_variable([self.state_dim, self.hide_layer_inputs])
+        b1 = self.bias_variable([self.hide_layer_inputs])
+        W2 = self.weight_variable([self.hide_layer_inputs, self.action_dim])
+        b2 = self.bias_variable([self.action_dim])
+        # input layer
+        self.state_input = tf.placeholder("float", [None, self.state_dim])
+        # hidden layers
+        h_layer = tf.nn.relu(tf.matmul(self.state_input, W1) + b1)
+        # Q Value layer
+        self.Q_value = tf.matmul(h_layer, W2) + b2
+        # 保存权重
+        self.Q_Weihgts = [W1, b1, W2, b2]
+
+    def create_TargetQ_network(self):
+        # network weights
+        W1 = self.weight_variable([self.state_dim, self.hide_layer_inputs])
+        b1 = self.bias_variable([self.hide_layer_inputs])
+        W2 = self.weight_variable([self.hide_layer_inputs, self.action_dim])
+        b2 = self.bias_variable([self.action_dim])
+        # input layer
+        # self.state_input = tf.placeholder("float",[None,self.state_dim])
+        # hidden layers
+        h_layer = tf.nn.relu(tf.matmul(self.state_input, W1) + b1)
+        # Q Value layer
+        self.TargetQ_value = tf.matmul(h_layer, W2) + b2
+        self.TargetQ_Weights = [W1, b1, W2, b2]
+
+    def copyWeightsToTarget(self):
+        for i in range(len(self.Q_Weihgts)):
+            self.session.run(tf.assign(self.TargetQ_Weights[i], self.Q_Weihgts[i]))
+
+    def create_training_method(self):
+        self.action_input = tf.placeholder("float", [None, self.action_dim])  # one hot presentation
+        self.y_input = tf.placeholder("float", [None])
+
+        Q_action = tf.reduce_sum(tf.multiply(self.Q_value, self.action_input), reduction_indices=1)  # mul->matmul
+        self.cost = tf.reduce_mean(tf.square(self.y_input - Q_action))
+        self.optimizer = tf.train.AdamOptimizer(0.001).minimize(self.cost)
+
+    def perceive(self, state, action, reward, next_state, done):
+        one_hot_action = np.zeros(self.action_dim)
+        one_hot_action[action] = 1
+        self.replay_buffer.append([state, one_hot_action, reward, next_state, done])
+        if len(self.replay_buffer) > REPLAY_SIZE:
+            self.replay_buffer.popleft()
+
+        if len(self.replay_buffer) > BATCH_SIZE:
+            self.train_Q_network()
+
+    def modify_last_reward(self, new_reward):
+        v = self.replay_buffer.pop()
+        v[2] = new_reward
+        self.replay_buffer.append(v)
+
+    def train_Q_network(self):
+        self.time_step += 1
+        # Step 1: obtain random minibatch from replay memory
+        minibatch = random.sample(self.replay_buffer, BATCH_SIZE)
+        state_batch = [data[0] for data in minibatch]
+        action_batch = [data[1] for data in minibatch]
+        reward_batch = [data[2] for data in minibatch]
+        next_state_batch = [data[3] for data in minibatch]
+
+        # Step 2: calculate y
+        y_batch = []
+        Q_value_batch = self.Q_value.eval(feed_dict={self.state_input: next_state_batch})
+        # Q_value_batch = self.TargetQ_value.eval(feed_dict={self.state_input:next_state_batch})
+        for i in range(0, BATCH_SIZE):
+            done = minibatch[i][4]
+            if done:
+                y_batch.append(reward_batch[i])
+            else:
+                y_batch.append(reward_batch[i] + GAMMA * np.max(Q_value_batch[i]))
+
+        self.optimizer.run(feed_dict={
+            self.y_input: y_batch,
+            self.action_input: action_batch,
+            self.state_input: state_batch
+        })
+
+        # 同步目标网络
+        if self.time_step % self.target_q_step == 0:
+            self.copyWeightsToTarget()
+
+    def egreedy_action(self, state):
+        Q_value = self.Q_value.eval(feed_dict={
+            self.state_input: [state]
+        })[0]
+
+        self.epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / 10000
+
+        if random.random() <= self.epsilon:
+            return np.random.randint(0, 4)
+        # return random.randint(0,self.action_dim - 1)
+        else:
+            return np.argmax(Q_value)
+
+    def action(self, state):
+        Q_value = self.Q_value.eval(feed_dict={
+            self.state_input: [state]
+        })[0]
+        return np.argmax(Q_value)
+
+    def weight_variable(self, shape):
+        initial = tf.truncated_normal(shape)
+        return tf.Variable(initial)
+
+    def bias_variable(self, shape):
+        initial = tf.constant(0.01, shape=shape)
+        return tf.Variable(initial)
+
+
+# ---------------------------------------------------------
+# Hyper Parameters
+EPISODE = 10000  # Episode limitation
+STEP = 300  # Step limitation in an episode
+TEST = 1  # The number of experiment test every 100 episode
+
+from gym.envs.registration import register
+
+register(
+    id='Snake-v0',
+    entry_point='snake:Snake',  # 第一个myenv是文件夹名字，第二个myenv是文件名字，MyEnv是文件内类的名字
+)
+
+
+def main():
+    # initialize OpenAI Gym env and dqn agent
+    env = gym.make('Snake-v0')
+    agent = DQN(env)
+
+    # agent.copyWeightsToTarget()
+    state = env.reset()
+    state = np.reshape(state, [-1])
+
+
+    while True:
+
+        env.render()
+
+        action = agent.action(state)
+        next_state, reward, done, length = env.step(action)
+
+        state = np.reshape(next_state, [-1])
+        time.sleep(0.1)
+
+        if done:
+            break
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ dqn_game/snake3/snake.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import gym
+from gym import spaces
+from gym.utils import seeding
+import numpy as np
+
+import pygame,sys,time,random
+from pygame.locals import *
+# 定义颜色变量
+redColour = pygame.Color(255,0,0)
+blackColour = pygame.Color(0,0,0)
+whiteColour = pygame.Color(255,255,255)
+greyColour = pygame.Color(150,150,150)
+
+class Snake(gym.Env):
+    """
+    """
+    def __init__(self):
+        self.viewer = None
+        self.fpsClock = pygame.time.Clock()
+        # 创建pygame显示层
+        self.playSurface = pygame.display.set_mode((320, 240))
+        self.step_num = 0
+
+        self.snakePosition = [100, 100]
+        self.snakeSegments = [[100, 100], [80, 100], [60, 100]]
+        self.raspberryPosition = [80, 80]
+        self.direction = 0
+
+        pygame.init()
+        pygame.display.set_caption('Raspberry Snake')
+
+    def build_data(self):
+        """
+        """
+        data = []
+        data.append(self.snakePosition)
+        data.append(self.raspberryPosition)
+        data.append([self.direction, 0])
+        data.extend(self.snakeSegments)
+        for i in range(len(data), 100):
+            data.append([0, 0])
+        return data
+
+        # image_data = pygame.surfarray.array3d(pygame.display.get_surface())
+        # return image_data
+
+    def step(self, action):
+        """
+        """
+        # 检测例如按键等pygame事件
+        pygame.event.get()
+
+        reward = 0
+        done = False
+
+        self.direction = action
+
+        # 根据方向移动蛇头的坐标
+        if self.direction == 0:
+            self.snakePosition[0] += 20
+        if self.direction == 1:
+            self.snakePosition[0] -= 20
+        if self.direction == 2:
+            self.snakePosition[1] -= 20
+        if self.direction == 3:
+            self.snakePosition[1] += 20
+        # 增加蛇的长度
+
+        raspberrySpawned = 1
+        self.snakeSegments.insert(0, list(self.snakePosition))
+        # 判断是否吃掉了树莓
+        if self.snakePosition[0] == self.raspberryPosition[0] and self.snakePosition[1] == self.raspberryPosition[1]:
+            raspberrySpawned = 0
+        else:
+            self.snakeSegments.pop()
+
+        self.step_num += 1
+        reward -= self.step_num * 0.2
+        # 如果吃掉树莓，则重新生成树莓
+        if raspberrySpawned == 0:
+            x = random.randrange(7, 9)
+            y = random.randrange(7, 9)
+            self.raspberryPosition = [int(x * 20), int(y * 20)]
+
+            reward += 1000
+
+        # 判断是否死亡
+        if self.snakePosition[0] > 300 or self.snakePosition[0] < 0:
+            self.reset()
+            reward = -10
+            done = True
+
+        if self.snakePosition[1] > 220 or self.snakePosition[1] < 0:
+            self.reset()
+            reward = -10
+            done = True
+
+        for snakeBody in self.snakeSegments[1:]:
+            if self.snakePosition[0] == snakeBody[0] and self.snakePosition[1] == snakeBody[1]:
+                self.reset()
+                reward = -10
+                done = True
+
+        # 控制游戏速度
+        # self.fpsClock.tick(5)
+
+        return self.build_data(), reward, done, len(self.snakeSegments) - 3
+
+    def reset(self):
+        self.snakePosition = [100, 100]
+        self.snakeSegments = [[100, 100], [80, 100], [60, 100]]
+        self.raspberryPosition = [80, 80]
+        self.direction = 0
+        self.step_num = 0
+        return self.build_data()
+
+
+    def render(self, mode='human', close=False):
+        # 绘制pygame显示层
+        self.playSurface.fill(blackColour)
+        for position in self.snakeSegments:
+            pygame.draw.rect(self.playSurface, whiteColour, Rect(position[0], position[1], 20, 20))
+            pygame.draw.rect(self.playSurface, redColour, Rect(self.raspberryPosition[0], self.raspberryPosition[1], 20, 20))
+
+        # 刷新pygame显示层
+        pygame.display.flip()
+        # self.fpsClock.tick(5)
+
+        # from gym.envs.classic_control import rendering
+        # viewer = rendering.Viewer(640, 480)
+        #
+        # return viewer.render(return_rgb_array=mode == 'rgb_array')
